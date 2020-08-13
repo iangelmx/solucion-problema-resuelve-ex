7709b43578730ab0520e90f8dfd0e66c27a6ef67
iangelmx <aramireza1506@alumno.ipn.mx>
2020-08-13 01:18:18 -0500

    Adding some tests

--- resuelve/test/resuelve/contexts/pruebas_first.exs
+++
@@ -1,8 +0,0 @@
-defmodule Resuelve.Contexts.PruebasFirst do
-  use ExUnit.Case
-  doctest Resuelve.Contexts.PruebasFirst
-
-  test  do
-    assert Resuelve.Contexts.PruebasFirst.hello() == :world
-  end
-end
---
+++ resuelve/test/resuelve/contexts/resources.exs
@@ -0,0 +1,15 @@
+defmodule New.Tests.Resources do
+  alias Resuelve.Context.Jugador
+
+  p1 = %Jugador{
+    nombre: "Ramíro",
+    nivel: nil,
+    goles: -5,
+    sueldo: "10",
+    bono: -10,
+    sueldo_completo: "hey",
+    equipo: "nuevo",
+  }
+
+
+end
