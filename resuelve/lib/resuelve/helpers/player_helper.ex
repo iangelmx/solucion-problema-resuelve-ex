24b221fd978e932420c66d1c23ba104ecf98d929
iangelmx <aramireza1506@alumno.ipn.mx>
2020-08-17 01:36:24 -0500

    Finishing first team compliance

--- resuelve/lib/resuelve/helpers/player_helper.ex
+++ resuelve/lib/resuelve/helpers/player_helper.ex
@@ -4,8 +4,8 @@ defmodule Resuelve.Helpers.PlayerHelper do
 
 
   @spec get_minimum_player_goals( map(), charlist()|nil ) :: integer()
-  def get_minimum_player_goals( players, team_name) do
-    Enum.map( players, fn(player) ->
+  def get_minimum_player_goals( player_list, team_name) do
+    Enum.map( player_list, fn(player) ->
       min_goals = LevelManager.get_minimum_goals_by_level( player.nivel, team_name )
       Map.put(player, :min_goals, min_goals)
     end)
@@ -23,33 +23,74 @@ defmodule Resuelve.Helpers.PlayerHelper do
   end
 
   @spec get_single_compliance_players( list( map()), charlist()) :: list( map() )
-  def get_single_compliance_players( players, team_name ) do
-    get_minimum_player_goals( players, team_name )
+  def get_single_compliance_players( player_list, team_name ) do
+    get_minimum_player_goals( player_list, team_name )
     |> Enum.map( fn(player)->
         s_c = calculate_single_compliance_player(player)
         Map.put( player, :single_compliance, s_c)
     end)
   end
 
-
-
   @spec calculate_complete_salary_for_players( map(), charlist()|nil) :: list( map() )
-  def calculate_teams_compliance( players ) do
-    by_team = Enum.group_by( players, fn(player) -> player.equipo end)
+  def calculate_teams_compliance( player_list ) do
+    by_team = Enum.group_by( player_list, fn(player) -> player.equipo end)
     teams = Map.keys( by_team )
-    #IO.inspect(by_team, label: "By team")
-    #IO.inspect(teams, label: "Teams")
     Enum.map( teams, fn(team) ->
-      IO.inspect( team, label: "Current team")
       Map.fetch!( by_team, team )
-      |> TeamHelper.calculate_compliance_by_team
+      |> TeamHelper.get_compliance_by_team
     end )
-
   end
 
-  def calculate_global_compliance( players, team_name) do
-    players = get_single_compliance_players( players, team_name )
-    calculate_teams_compliance( players)
+  @spec calculate_global_single_player(map()) :: float()
+  def calculate_global_single_player( %{single_compliance: sc, team_compliance: tc} ) do
+    (sc + tc)/2
+  end
+
+  @spec calculate_global_compliance(list( map() )) :: list(map())
+  def calculate_global_compliance( player_list ) do
+    Enum.map( player_list, fn(player) ->
+      g_c = calculate_global_single_player(player)
+      Map.put(player, :global_compliance, g_c)
+    end)
+  end
+
+  @spec get_global_compliance_players(list(map()), charlist()) :: list(map())
+  def get_global_compliance_players( player_list, team_name) do
+    get_single_compliance_players( player_list, team_name )
+    |> calculate_teams_compliance
+    |> List.flatten
+    |> calculate_global_compliance
+  end
+
+  @spec calculate_final_salary( map() ) :: number()
+  def calculate_final_salary( %{bono: bonus, sueldo: salary, global_compliance: g_c} ) do
+    salary + ((bonus * g_c) / 100)
+  end
+
+  def get_final_salary( player_list ) do
+    Enum.map( player_list, fn(player)->
+      salary = calculate_final_salary(player)
+      Map.put( player, :sueldo_completo, salary)
+    end)
+  end
+
+  @spec take_only_necessary_attrs(map()) :: map()
+  def take_only_necessary_attrs(player) do
+    Map.take(player, [
+      :nombre,
+      :nivel,
+      :goles_minimos,
+      :goles,
+      :sueldo,
+      :bono,
+      :sueldo_completo,
+      :equipo
+    ])
+  end
+
+  @spec format_output( list(map())) :: list( map() )
+  def format_output( player_list ) do
+    Enum.map( player_list, fn(player) -> take_only_necessary_attrs(player) end)
   end
 
   @spec calculate_complete_salary_for_players(list(map()), charlist()|nil) :: list()
@@ -57,11 +98,10 @@ defmodule Resuelve.Helpers.PlayerHelper do
     IO.puts("HEy...")
     # Enum.map(player_list)
     # Calcula alcance global
-    calculate_global_compliance( player_list, team_name )
-    # Calcula alcande individual
-    # Calcula alcance grupal
-    # Calcula salario final
-    # Formatea la salida
+    get_global_compliance_players( player_list, team_name )
+    |> get_final_salary
+    |> format_output
+
   end
 
 end
--- resuelve/lib/resuelve/helpers/team_helper.ex
+++ resuelve/lib/resuelve/helpers/team_helper.ex
@@ -7,9 +7,23 @@ defmodule Resuelve.Helpers.TeamHelper do
     Enum.reduce( players_team, 0, fn player, acc -> player.min_goals + acc end )
   end
 
-  def calculate_compliance_by_team( players_team ) do
-    get_scored_goals_by_team( players_team )
-    #IO.inspect()
+  @spec calculate_complicance_by_team(integer(), integer()) :: float
+  def calculate_complicance_by_team( scored_goals, minimum_goals ) when scored_goals >= minimum_goals do
+    100.0
+  end
+  def calculate_complicance_by_team( scored_goals, minimum_goals ) when scored_goals < minimum_goals and minimum_goals >0 do
+    scored_goals * 100 / minimum_goals
+  end
+  def calculate_complicance_by_team( _, _ ) do
+    0.0
+  end
+
+  @spec get_compliance_by_team(list(map())) :: list()
+  def get_compliance_by_team( players_team ) do
+    scored = get_scored_goals_by_team( players_team )
+    minimum = get_minimum_goals_by_team ( players_team )
+    t_c = calculate_complicance_by_team( scored, minimum )
+    Enum.map( players_team, fn(player) -> Map.put(player, :team_compliance, t_c) end)
   end
 
 end
